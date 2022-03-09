c730d8e7a8d2abbc71e985581566ed8e83831122
lbolanos90 <lcbolanos07@gmail.com>
2022-03-09 13:55:36 -0300

    updated JQuery for to do list
    I had to use the notes at the bottom. Added my notes with my attempts and mistakes.

--- JS/to-do-list.js
+++ JS/to-do-list.js
@@ -5,12 +5,11 @@ function newItem(){
 let li = $('<li></li>');
 let inputValue = $('#input').val(); 
 li.append(inputValue);
-
 /*Turns out this was unnecessary=>   $('inputValue').click(function (){
     let inputValue = $('#input').val();
 */
-    //What I learned with Edwards help.. 
-    if(inputValue === " "){
+//What I learned with Edwards helped.. 
+    if(inputValue === ''){
         alert("You must write something!");
     }else{
         $('#list').append(li);
@@ -21,27 +20,42 @@ li.append(inputValue);
     }
     */
 
-
 //2 Crossing out an item from the list of items:
 function crossOut() {
-    li.addClass("strike");
+    li.toggleClass("strike");
 }
-$('li').on("dblclick", crossOut); 
+
+li.on("dblclick", function crossOut(){
+    li.toggleClass("strike");
+});
 
 //3(i) Adding the delete button "X": 
-let  crossOutButton = $('<button>X</button>');
-$('button').append(addEventListener("dblclick",crossOut));
+let  crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
 li.append(crossOutButton);
+
+crossOutButton.on('click', deleteListItem);
+/* my attempt 
+$(crossOutButton).on('click', function()){
+    li.remove();
+}
+*/ 
 /* not sure how I'm supposed to use this.  
 deleteButton.append(document.createTextNode('X'));
 */ 
 
 //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
 function deleteListItem(){
-    let li = $('<li></li>');
-    li.addClass("delete");
+    //why not crossOutButton? Is is because li is appended to crossOutButton?
+    li.addClass('delete');
 }
+// $('#input').on('click', function () {
+//     //let li = $('<li></li>');
+//     li.addClass("delete");
+//   });
 
 // 4. Reordering the items: 
 $('#list').sortable();
 }
+
+newItem();
